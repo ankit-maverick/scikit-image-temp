a116f9f6fc8de12b55fe54239f75bfca12323d3e
Ankit Agrawal <aaaagrawal@iitb.ac.in>
2013-08-13 20:04:04 +0530

    Making compatible to new changes

--- plot_censure_features.py
+++ plot_censure_features.py
@@ -1,31 +1,37 @@
-from skimage.feature import censure_keypoints
-from skimage.color import rgb2gray
+from skimage.feature import keypoints_censure
 from skimage.data import lena
+from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
-img = lena()
 
 # Initializing the parameters for Censure keypoints
+img = lena()
 gray_img = rgb2gray(img)
-n_scales = 7
-mode = 'DoB'
-nms_threshold = 0.10
+min_scale = 1
+max_scale = 7
+nms_threshold = 0.15
 rpc_threshold = 10
-kp_star, scale = censure_keypoints(gray_img, n_scales, mode, nms_threshold,
-                      rpc_threshold)
 
-f, axarr = plt.subplots(2, n_scales / 2)
 
-# Plotting Censure features at all the scales
-for i in range(n_scales - 2):
-    keypoints = kp_star[scale == i + 2]
-    num = len(keypoints)
-    x = keypoints[:, 1]
-    y = keypoints[:, 0]
-    s = 5 * 2**(i + 2)
-    axarr[i / 3, i - (i / 3) * 3].imshow(img)
-    axarr[i / 3, i - (i / 3) * 3].scatter(x, y, s, facecolors='none',
-                                          edgecolors='g')
-    axarr[i / 3, i - (i / 3) * 3].set_title(' %s %s-Censure features at scale %d' % (num, mode, i + 2))
+# Plotting features for the following modes
+for mode in ['dob', 'octagon', 'star']:
 
-plt.suptitle('NMS threshold = %f, RPC threshold = %d' % (nms_threshold, rpc_threshold))
+    kp_censure, scale = keypoints_censure(gray_img, min_scale, max_scale,
+                                          mode, nms_threshold,rpc_threshold)
+    f, axarr = plt.subplots((max_scale - min_scale + 1) // 3, 3)
+
+    # Plotting Censure features at all the scales
+    for i in range(max_scale - min_scale - 1):
+        keypoints = kp_censure[scale ==  i + min_scale + 1]
+        num = len(keypoints)
+        x = keypoints[:, 1]
+        y = keypoints[:, 0]
+        s = 5 * 2**(i + min_scale + 1)
+        axarr[i // 3, i - (i // 3) * 3].imshow(img)
+        axarr[i // 3, i - (i // 3) * 3].scatter(x, y, s, facecolors='none',
+                                                edgecolors='g')
+        axarr[i // 3, i - (i // 3) * 3].set_title(' %s %s-Censure features at '
+                                                  'scale %d' % (num, mode, i +
+                                                                min_scale + 1))
+
+    plt.suptitle('NMS threshold = %f, RPC threshold = %d' % (nms_threshold, rpc_threshold))
 plt.show()
